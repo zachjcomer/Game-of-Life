e482d07f1e740dd23b1165a8d9900bbea5e0857e
zachjcomer <zachjcomer@gmail.com>
2022-07-07 20:11:22 -0400

    bookkeeping methods, cell neighbors

--- cell.py
+++ cell.py
@@ -17,13 +17,22 @@ class cell:
         '''
         Return the cell location and the cell state.
         '''
-        return f'({self.coords[0]},{self.coords[1]}): {self.state}.'
+        return f'({self.coords[0]},{self.coords[1]}): {self.state}'
+
+    def getCoords(self):
+        '''
+        Return the cell location.
+        '''
+        return self.coords
 
     def getNeighbors(self):
         '''
         Return a list of the current cell's neighbor cell objects.
         '''
-        return self.neighbors
+        s = []
+        for neighbor in self.neighbors:
+            s.append(neighbor.getCoords())
+        return s
 
     def addNeighbor(self, neighbor):
         '''
@@ -31,13 +40,32 @@ class cell:
         '''
         self.neighbors.append(neighbor)
 
+    def getNeighborSum(self):
+        neighborSum = 0
+        for cell in self.neighbors:
+            neighborSum += cell.getState()
+        return neighborSum
+
     def getState(self):
         '''
         Return the current state of the cell.
         '''
         return self.state
+    
+    def isAlive(self):
+        '''
+        Returns true if the cell is alive.
+        '''
+        return self.state == 1
 
-    def setState(self):
+    def setState(self, x):
+        '''
+        Sets the value of the cell.
+        '''
+        self.state = x
+        return None
+
+    def toggleState(self):
         '''
         Toggle the state of the cell.
         '''
